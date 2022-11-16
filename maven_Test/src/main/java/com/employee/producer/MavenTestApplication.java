b2dfad177f1bc7e34311fb3c3b540fea1146022c
Abhinav Singh <abhisinghbhu55@gmail.com>
2022-11-16 23:19:38 +0530

    First

---
+++ maven_Test/src/main/java/com/employee/producer/MavenTestApplication.java
@@ -0,0 +1,15 @@
+package com.employee.producer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavenTestApplication {
+	
+
+	public static void main(String[] args) {
+		System.out.println("Hello Hello");
+		SpringApplication.run(MavenTestApplication.class, args);
+	}
+
+}
